9ccd7f84716d3f416d200e51e87a530e9c536339
nannullna <nannullna00@gmail.com>
2021-10-01 09:00:00 +0000

    added description

--- dataset/preprocessor/regex.py
+++ dataset/preprocessor/regex.py
@@ -11,6 +11,13 @@ class Regex:
         # 다만, 중복된 2단어 이상의 '구'는 제거하지 못합니다
         return Regex.pattern_remove_duplicated_words.sub(r'\1', txt)
 
-# 실험 예시
-txt = "this is a duplicated duplicated duplicated word from an error occured occured error occured"
-print(Regex.remove_duplicated_words(txt))
+def experiment():
+    # 실험 예시
+    # 원하는 텍스트 넣고 충분히 실험해보세요~
+    txt = "this is a duplicated duplicated duplicated word from an error occured occured error occured"
+    print(Regex.remove_duplicated_words(txt))
+
+if __name__ == '__main__':
+    # regex.py 파일을 실행시킬 경우에는 실험을 진행합니다~
+    # 나머지에서는 Regex class 가져다가 쓰시면 될 것 같아요
+    experiment()
